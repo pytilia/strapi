47d77762dec9ca733adaeb955e73d59606c7671d
Eileen O'Reilly <eileen.oreilly@pytilia.io>
2021-11-25 13:28:12 +0000

    Adding yaml files

--- strapi-web-app/scripts/before_install.sh
+++ strapi-web-app/scripts/before_install.sh
@@ -21,3 +21,18 @@ fi
 ansible-playbook $(dirname $0)/ansible/nginx.yaml --connection=local
 
 sudo rm -rf $DESTINATION_PATH/index.html
+
+cd ~
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs
+node -v && npm -v
+
+cd ~
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+
+sudo nano ~/.profile
+
+export PATH=~/.npm-global/bin:$PATH
+
+source ~/.profile
